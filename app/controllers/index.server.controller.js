e0df83dacfd8805dfcf174fa14a423a93c45da17
Jason McBride <jsononrails@gmail.com>
2015-03-09 00:39:21 -0400

    added articles

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,9 +1,8 @@
 exports.render = function(req, res) {
 	
 	req.session.lastVisit = new Date();
-	
 	res.render('index', {
 		title: 'Hello World',
-		user: JSON.stringify(req.user)
+		user:  JSON.stringify(req.user)
 	});
 };
