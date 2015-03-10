077ca4225c30d6e50cdb3fe5c890af9d3eea41af
Jason McBride <jsononrails@gmail.com>
2015-03-10 09:16:32 -0400

    rename

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -1,5 +1,7 @@
 angular.module('example').controller('ExampleController', ['$scope', 
 	'Authentication',
 	function($scope, Authentication) {
+		console.log("this " + Authentication);
 		$scope.authentication = Authentication;
-	}])
+		console.log($scope);
+	}]);
